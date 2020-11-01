098be30aa9d3d7ce7e4f4e86033df81378dc115c
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-11-01 19:32:31 +0100

    Event-Handler fuer Aenderungen RGB-Werte, schreibt aber nur Hex-Wert auf Konsole.

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 Simple [Ionic](https://ionicframework.com) app to demonstrate the UI component
 [ion-range](https://ionicframework.com/docs/api/range).
-The app contains three instances of `ion-range` to define a color by choosing the red, green and blue values.
+The app use three instances of `ion-range` to define a color by choosing the red, green and blue values (RGB).
 
 <br>
 
 ----
 ## License ##
 
-See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License)
-for the files in this repository.
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License) for the files in this repository.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  /** Wert für Rot-Anteil von Farbe (0..255). */
+  private rotWert   : number = 240;
+
+  /** Wert für Grün-Anteil von Farbe (0..255). */
+  private gruenWert : number = 10;
+
+  /** Wert für Balu-Anteil von Farbe (0..255). */
+  private blauWert  : number = 10;
+
   constructor() {}
+
+  /**
+   * Event-Handler-Methode, die aufgerufen wird, wenn einer der RGB-Farbwerte über
+   * eines der "ion-range"-Elemente geändert wird.
+   */
+  onFarbanteilChanged() {
+
+    const HEX_BASIS_VALUE = 16;
+
+    console.log(`rot=${this.rotWert}, grün=${this.gruenWert}, blau=${this.blauWert}`);
+
+    let rotHex   = this.rotWert.toString(   HEX_BASIS_VALUE );
+    let gruenHex = this.gruenWert.toString( HEX_BASIS_VALUE );
+    let blauHex  = this.blauWert.toString(  HEX_BASIS_VALUE );
+
+    // Jeder Hex-Wert muss zweistellig sein
+    if (rotHex.length   === 1) { rotHex   = "0" + rotHex;}
+    if (gruenHex.length === 1) { gruenHex = "0" + gruenHex;}
+    if (blauHex.length  === 1) { blauHex  = "0" + blauHex;}
+
+    const totalColorHex = rotHex + gruenHex + blauHex;
+
+    console.log(`Farbe=${totalColorHex}`);
+  }
 
 }
