299e89925522fea4a3103d9cf68485db3ad8a22f
mide_42b@yahoo.de <mide_42b@yahoo.de>
2020-11-01 21:02:58 +0100

    Ausgewaehlte Farbe wird in chip-Element angezeigt.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,7 +16,17 @@ export class HomePage {
   /** Wert für Balu-Anteil von Farbe (0..255). */
   private blauWert  : number = 10;
 
-  constructor() {}
+  /** Hex-Code der aktuelles ausgewählten Farbe. */
+  private farbeHexCode : string = "";
+
+  /**
+   * Konstruktor, führt Event-Handler-Methode aus.
+   */
+  constructor() {
+
+    this.onFarbanteilChanged();
+  }
+
 
   /**
    * Event-Handler-Methode, die aufgerufen wird, wenn einer der RGB-Farbwerte über
@@ -26,20 +36,20 @@ export class HomePage {
 
     const HEX_BASIS_VALUE = 16;
 
-    console.log(`rot=${this.rotWert}, grün=${this.gruenWert}, blau=${this.blauWert}`);
+    //console.log(`rot=${this.rotWert}, grün=${this.gruenWert}, blau=${this.blauWert}`);
 
     let rotHex   = this.rotWert.toString(   HEX_BASIS_VALUE );
     let gruenHex = this.gruenWert.toString( HEX_BASIS_VALUE );
     let blauHex  = this.blauWert.toString(  HEX_BASIS_VALUE );
 
     // Jeder Hex-Wert muss zweistellig sein
-    if (rotHex.length   === 1) { rotHex   = "0" + rotHex;}
-    if (gruenHex.length === 1) { gruenHex = "0" + gruenHex;}
-    if (blauHex.length  === 1) { blauHex  = "0" + blauHex;}
+    if (rotHex.length   === 1) { rotHex   = "0" + rotHex;   }
+    if (gruenHex.length === 1) { gruenHex = "0" + gruenHex; }
+    if (blauHex.length  === 1) { blauHex  = "0" + blauHex;  }
 
-    const totalColorHex = rotHex + gruenHex + blauHex;
+    this.farbeHexCode = "#" + rotHex + gruenHex + blauHex;
 
-    console.log(`Farbe=${totalColorHex}`);
+    document.body.style.setProperty("--gewaehlte-farbe", this.farbeHexCode);
   }
 
 }
