e2f268bd0836993845580d6a810080179c24ae8a
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-24 18:39:40 +0200

    Seite 3: Double Range

---
+++ src/app/LowerUpperWerte.ts
@@ -0,0 +1,11 @@
+
+/**
+ * Interface für die beiden Werte eines Intervalls, die
+ * mit einem Range (Slider) mit "dual knobs" dargestellt
+ * oder gesetzt werden.
+ */
+export interface LowerUpperWerte {
+
+  lower: number;
+  upper: number;
+}
---
+++ src/app/seite3/seite3.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { LowerUpperWerte } from '../LowerUpperWerte';
+
+
+/**
+ * Seite für Demo <ion-range> mit zwei Slidern.
+ */
+@Component({
+  selector: 'app-seite3',
+  templateUrl: './seite3.page.html',
+  styleUrls: ['./seite3.page.scss'],
+})
+export class Seite3Page {
+
+  public zweiWerte: LowerUpperWerte = { lower: 10,
+                                        upper: 90 };
+}
